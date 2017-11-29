6fe7fe508ce871860bf64188b7df789758eba1bc
Matte Szklarz <mszklarz@gmail.com>
2017-11-29 16:16:34 -0500

    add class on reveal functionality
    Get all elements with "unveil" class and add class supplied with data-class="" to the element.

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -1,6 +1,6 @@
 /**
  * jQuery Unveil
- * A very lightweight jQuery plugin to lazy load images
+ * A very lightweight jQuery plugin to lazy load imagess
  * http://luis-almeida.github.com/unveil
  *
  * Licensed under the MIT license.
@@ -8,45 +8,54 @@
  * https://github.com/luis-almeida
  */
 
-;(function($) {
+;
+(function($) {
 
   $.fn.unveil = function(threshold, callback) {
 
     var $w = $(window),
-        th = threshold || 0,
-        retina = window.devicePixelRatio > 1,
-        attrib = retina? "data-src-retina" : "data-src",
-        images = this,
-        loaded;
+      th = threshold || 0,
+      retina = window.devicePixelRatio > 1,
+      attrib = retina ? "data-src-retina" : "data-src",
+      items = this,
+      loaded;
 
     this.one("unveil", function() {
       var source = this.getAttribute(attrib);
-      source = source || this.getAttribute("data-src");
-      if (source) {
-        this.setAttribute("src", source);
-        if (typeof callback === "function") callback.call(this);
+      var className = this.getAttribute("data-class");
+      if (className) {
+         this.classList.add(className);
+      } else {
+        source = source || this.getAttribute("data-src");
+        if (source) {
+          this.setAttribute("src", source);
+          if (typeof callback === "function") callback.call(this);
+        }
       }
     });
 
+    //Check item array and see if items are inview,
+    //if so,
     function unveil() {
-      var inview = images.filter(function() {
+      var inview = items.filter(function() {
         var $e = $(this);
         if ($e.is(":hidden")) return;
 
         var wt = $w.scrollTop(),
-            wb = wt + $w.height(),
-            et = $e.offset().top,
-            eb = et + $e.height();
+          wb = wt + $w.height(),
+          et = $e.offset().top,
+          eb = et + $e.height();
 
         return eb >= wt - th && et <= wb + th;
       });
 
       loaded = inview.trigger("unveil");
-      images = images.not(loaded);
+      items = items.not(loaded);
     }
 
     $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
 
+    //run on init
     unveil();
 
     return this;
